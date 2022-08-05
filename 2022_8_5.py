7b302f18ad3ad0a5368663490470689163fe49d2
mxy <mxy2287352895@outlook.com>
2022-08-05 19:52:19 +0800

    .\2022_8_5.py

---
+++ 2022_8_5.py
@@ -0,0 +1,16 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2022/8/5 19:35
+# Author : 马县杨
+# @File : demo9.py
+# @software: PyCharm
+a=10
+lst=['Hello world',10]#列表存储多种不同数据
+print(id(a))
+print(type(lst))
+print(id(lst))
+print(id(lst[1]))
+lst=['Hello world',10,'Hello world']
+print(lst.index('Hello world'))#优先最近
+print(id(lst[0]),id(lst[2]))
+print(lst.index('Hello world',1,3))#左闭右开
+print(lst[2],lst[-1])#负数从后向前数
